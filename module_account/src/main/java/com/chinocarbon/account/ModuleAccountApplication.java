0e0734bcc6bf90fc8bee2abe96f4686d51784a76
chinocarbon <1070980757@qq.com>
2024-02-24 13:42:00 +0800

    增加用户账户模块

---
+++ module_account/src/main/java/com/chinocarbon/account/ModuleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.chinocarbon.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleAccountApplication.class, args);
+    }
+
+}
---
+++ module_account/src/test/java/com/chinocarbon/account/ModuleAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinocarbon.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
