8faa7a2cfd83f9f4dd895e2788e479adc9b8aaeb
chinocarbon <1070980757@qq.com>
2024-02-25 16:35:24 +0800

    add judgement modules

---
+++ module_judgement/src/main/java/com/chinocarbon/module_judgement/ModuleJudgementApplication.java
@@ -0,0 +1,13 @@
+package com.chinocarbon.module_judgement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleJudgementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleJudgementApplication.class, args);
+    }
+
+}
---
+++ module_judgement/src/test/java/com/chinocarbon/module_judgement/ModuleJudgementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chinocarbon.module_judgement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleJudgementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
